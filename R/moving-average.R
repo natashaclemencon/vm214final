98b1cf41122cbb202212c1eac947926af3d078ef
veronica-moran <moranv620@gmail.com>
2026-08-27 12:36:53 -0700

    ma changes

--- R/moving-average.R
+++ R/moving-average.R
@@ -2,14 +2,16 @@ library(tidyverse)
 
 # Creating the function for moving average to insert into spaghetti script
 
-#  The input to this function should be a data frame containing stream chemistry data
+# The input to this function should be a data frame containing stream chemistry data
 moving_average <- function(bpr_ions) {
   # Initialize a tibble to contain the results
   result <- tibble(
-    window_start = seq(bpr_ions$Sample_Date[1],
-    bpr_ions$Sample_Date[nrow(bpr_ions)],
-    by = "9 weeks"),
-     K = NA,
+    window_start = seq(
+      bpr_ions$Sample_Date[1],
+      bpr_ions$Sample_Date[nrow(bpr_ions)],
+      by = "9 weeks"
+    ),
+    K = NA,
     `NO3-N` = NA,
     Mg = NA,
     Ca = NA,
@@ -17,7 +19,7 @@ moving_average <- function(bpr_ions) {
     # Fill in the rest of the ions
   )
 
-# Fill in the iterator and sequence
+  # Fill in the iterator and sequence
   for (i in 1:nrow(result)) {
     # Create variables for the start and end of the current window
     w1 <- result$window_start[i]
@@ -41,43 +43,40 @@ moving_average <- function(bpr_ions) {
     result$Mg[i] <- mean(mg_window, na.rm = TRUE)
     result$Ca[i] <- mean(ca_window, na.rm = TRUE)
     result$`NH4-N`[i] <- mean(nh4n_window, na.rm = TRUE)
-
-    
   }
   return(result)
 }
-
 
 ## tested code
 
 # The input to this function should be a data frame containing stream chemistry data
 
 # Fill in the iterator and sequence
-  #for (i in 1:nrow(result)) {
-    # Create variables for the start and end of the current window
-   # w1 <- result$window_start[i]
-    #w2 <- w1 + 63
+#for (i in 1:nrow(result)) {
+# Create variables for the start and end of the current window
+# w1 <- result$window_start[i]
+#w2 <- w1 + 63
 
-    # Create a logical vector, called "in_window", that says which samples are inside the window
-    # Hint: you'll compare sample dates to the start and end of the window
-    #in_window <- (bpr_ions$Sample_Date >= w1 & bpr_ions$Sample_Date < w2)
+# Create a logical vector, called "in_window", that says which samples are inside the window
+# Hint: you'll compare sample dates to the start and end of the window
+#in_window <- (bpr_ions$Sample_Date >= w1 & bpr_ions$Sample_Date < w2)
 
-    # Use indexing to pull out the ion concentrations that fall inside the window
-    #k_window <- bpr_ions$K[in_window]
-    #no3n_window <- bpr_ions$`NO3-N`[in_window]
-    #mg_window <- bpr_ions$Mg[in_window]
-    #ca_window <- bpr_ions$Ca[in_window]
-    #nh4n_window <- bpr_ions$`NH4-N`[in_window]
-    # The line above gets potassium in the window. Get the rest of the ions too
+# Use indexing to pull out the ion concentrations that fall inside the window
+#k_window <- bpr_ions$K[in_window]
+#no3n_window <- bpr_ions$`NO3-N`[in_window]
+#mg_window <- bpr_ions$Mg[in_window]
+#ca_window <- bpr_ions$Ca[in_window]
+#nh4n_window <- bpr_ions$`NH4-N`[in_window]
+# The line above gets potassium in the window. Get the rest of the ions too
 
-    # Calculate the mean of each ion concentration and fill in the result
-    #result$K[i] <- mean(k_window, na.rm = TRUE)
-    #result$`NO3-N`[i] <- mean(no3n_window, na.rm = TRUE)
-    #result$Mg[i] <- mean(mg_window, na.rm = TRUE)
-    #result$Ca[i] <- mean(ca_window, na.rm = TRUE)
-    #result$`NH4-N`[i] <- mean(nh4n_window, na.rm = TRUE)
+# Calculate the mean of each ion concentration and fill in the result
+#result$K[i] <- mean(k_window, na.rm = TRUE)
+#result$`NO3-N`[i] <- mean(no3n_window, na.rm = TRUE)
+#result$Mg[i] <- mean(mg_window, na.rm = TRUE)
+#result$Ca[i] <- mean(ca_window, na.rm = TRUE)
+#result$`NH4-N`[i] <- mean(nh4n_window, na.rm = TRUE)
 
-    #return(result)
-  #}
+#return(result)
+#}
 
 # end tested code
